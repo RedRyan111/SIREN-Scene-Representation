46e56530bac65ec4d1634f4584869da1a66f9935
RedRyan111 <ravenengineerryan@gmail.com>
2023-12-16 23:26:09 -0600

    Create transformations.py

---
+++ transformations/transformations.py
@@ -0,0 +1,15 @@
+import torch
+
+
+class CameraToWorldSpatialTransformationManager:
+    def __init__(self, spatial_matrix):
+        self.spatial_matrix = spatial_matrix
+        self.orientation = spatial_matrix[:3, :3]
+        self.translation = spatial_matrix[:3, -1]
+
+    def transform_ray_bundle(self, ray_bundle):
+        return torch.matmul(ray_bundle, self.orientation.T)
+
+    def expand_origin_to_match_ray_bundle_shape(self, ray_bundle):
+        return self.translation.expand(ray_bundle.shape)
+
