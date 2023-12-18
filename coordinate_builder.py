a91fa6696a75044231d5608e4154a698a65d1e76
RedRyan111 <ravenengineerryan@gmail.com>
2023-12-18 06:34:09 -0600

    Add inverse spatial matrices

--- coordinate_builder.py
+++ coordinate_builder.py
@@ -1,11 +1,24 @@
 import torch
 
+from transformations.transformations import CameraToWorldSpatialTransformationManager
+
 
 def camera_ray_to_plucker_ray(camera_to_world_matrix, ray_origin, ray_direction):
-
-
     camera_origin = camera_to_world_matrix
 
     torch.cross(camera_origin, ray_direction)
+
+    return 0
+
+
+def my_ray_representation(camera_to_world_matrix):
+    cam_to_world_obj = CameraToWorldSpatialTransformationManager(camera_to_world_matrix)
+    origin = cam_to_world_obj.translation
+
+    projection = torch.zeros(3)
+
+    return origin, projection
+
+def project_ray(ray_origin, ray_direction):
 
     return 0
--- transformations/transformations.py
+++ transformations/transformations.py
@@ -6,6 +6,17 @@ class CameraToWorldSpatialTransformationManager:
         self.spatial_matrix = spatial_matrix
         self.orientation = spatial_matrix[:3, :3]
         self.translation = spatial_matrix[:3, -1]
+        self.inverse_spacial_matrix = self.build_inverse_spatial_matrix()
+
+    def build_inverse_spatial_matrix(self):
+        inverse_spacial_matrix = torch.eye(4)
+        inverse_rotation = self.orientation.T
+        inverse_translation = -1 * inverse_rotation * self.translation
+
+        inverse_spacial_matrix[:3, :3] = inverse_rotation
+        inverse_spacial_matrix[:3, -1] = inverse_translation
+
+        return inverse_spacial_matrix
 
     def transform_ray_bundle(self, ray_bundle):
         return torch.matmul(ray_bundle, self.orientation.T)
@@ -16,6 +27,9 @@ class CameraToWorldSpatialTransformationManager:
 # camera to world
 
 # camera to camera
+def camera_to_camera(starting_camera_to_world: CameraToWorldSpatialTransformationManager, final_camera_to_world_2: CameraToWorldSpatialTransformationManager):
+    starting_inverse_camera_to_world = starting_camera_to_world.inverse_spacial_matrix
+    return torch.matmul(starting_inverse_camera_to_world, final_camera_to_world_2.spatial_matrix)
 
 
 # world to camera
